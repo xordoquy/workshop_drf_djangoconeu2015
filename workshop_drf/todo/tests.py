6b3682f69d40a721f4fc52bf14c7200c30fc4491
Xavier Ordoquy <xordoquy@linovia.com>
2015-06-04 00:43:38 +0100

    Add a simple test.

--- workshop_drf/todo/tests.py
+++ workshop_drf/todo/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+from django.core.urlresolvers import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from django.contrib.auth import get_user_model
+from . import models
 
-# Create your tests here.
+
+User = get_user_model()
+
+class TestTask(APITestCase):
+
+    def test_task_creation(self):
+        user = User.objects.create(username="admin")
+        category = models.Category.objects.create(name="Django")
+        url = reverse('task-list')
+        data = {
+            "name": "demo",
+            "owner": user.id,
+            "categories": [category.id],
+            "done": False,
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(
+            response.status_code, status.HTTP_201_CREATED, response.content)
+        data['id'] = 1
+        self.assertEqual(response.data, data)
+        self.assertEqual(category.tasks.count(), 1)
+
