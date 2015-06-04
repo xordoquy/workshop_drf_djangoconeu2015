b13c8b5cd0dde5d329a14b99c672307567992434
Xavier Ordoquy <xordoquy@linovia.com>
2015-06-04 01:05:27 +0100

    Add human readable owner & categories.

--- workshop_drf/todo/serializers.py
+++ workshop_drf/todo/serializers.py
@@ -1,13 +1,24 @@
 from rest_framework import serializers
+from django.contrib.auth import get_user_model
 from . import models
 
 
 class Category(serializers.ModelSerializer):
     class Meta:
         model = models.Category
+        fields = ('id', 'name')
 
 
 class Task(serializers.ModelSerializer):
+    owner = serializers.SlugRelatedField(
+        slug_field='username',
+        queryset=get_user_model().objects.all())
+    categories = serializers.SlugRelatedField(
+        slug_field='name',
+        queryset=models.Category.objects.all(),
+        many=True)
+
     class Meta:
         model = models.Task
+        fields = ('id', 'name', 'owner', 'categories', 'done')
 
